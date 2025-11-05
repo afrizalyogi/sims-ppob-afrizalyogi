1f7c38bd9698c2e95b6295f91489bd3ad3ff3633
Afrizal Yogi <afrizalyogi.id@gmail.com>
2025-11-05 22:52:59 +0700

    Initial Project Structure. Using router, redux, tailwind, axios

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Profile from "./app/Profile/page";
+import Home from "./app/Home/page";
+import Registration from "./app/Auth/Registration/page";
+import Login from "./app/Auth/Login/page";
+import TransactionTopup from "./app/Transaction/Topup/page";
+import TransactionPayment from "./app/Transaction/Payment/page";
+import TransactionHistory from "./app/Transaction/History/page";
+import NotFound from "./app/NotFound/page";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/registration", element: <Registration /> },
+  { path: "/login", element: <Login /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/profile/image", element: "Hello World!" },
+  { path: "/profile/update", element: "Hello World!" },
+  { path: "/topup", element: <TransactionTopup /> },
+  { path: "/payment", element: <TransactionPayment /> },
+  { path: "/history", element: <TransactionHistory /> },
+  { path: "*", element: <NotFound /> },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/app/Transaction/Topup/page.tsx
@@ -0,0 +1,3 @@
+export default function TransactionTopup() {
+  return "Hello World!";
+}
