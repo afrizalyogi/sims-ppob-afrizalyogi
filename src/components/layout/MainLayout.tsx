f1c45424b1da971614efe7cb357c0e9ebb6a36c8
Afrizal Yogi <afrizalyogi.id@gmail.com>
2025-11-08 09:49:02 +0700

    Minor Fix Move Mobile Gap from Body to Layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 pb-16 sm:pb-0">
       <Navbar />
       <main className="flex grow">{children}</main>
     </div>
